9d9d2f4093c028e43818c51b78045ec6f7befbfb
cmiranda <obando.mo.miguel@gmail.com>
2012-05-21 13:29:36 +0200

    Segundo COMMIT : Se ha añadido el método "metodo_rama1_clase1" en las clase 1

--- cmiranda/fuentes/clase1.java
+++ cmiranda/fuentes/clase1.java
@@ -10,4 +10,9 @@ public class clase1 {
 	{
 		System.out.println("Método 1 de la Clase 1");
 	}
+	
+	public void metodo_rama1_clase1()
+	{
+		System.out.println("Método Rama 1 de la clase 1");
+	}
 }
