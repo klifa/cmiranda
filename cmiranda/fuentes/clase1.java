9676dc926bff19a8d4e0a4aa7e42bfd370076f44
cmiranda <obando.mo.miguel@gmail.com>
2012-05-21 13:22:58 +0200

    Este primer commit incluye : "método1_Clase1" y "método2_Clase2"

---
+++ cmiranda/fuentes/clase1.java
@@ -0,0 +1,13 @@
+
+public class clase1 {
+	
+	public void imprimirEnPantalla()
+	{
+		System.out.println("Soy la clase 1");
+	}
+	
+	public void metodo1_clase1()
+	{
+		System.out.println("Método 1 de la Clase 1");
+	}
+}
---
+++ cmiranda/fuentes/clase2.java
@@ -0,0 +1,15 @@
+
+public class clase2 {
+	
+	public void ImprimirEnPantalla()
+	{
+		System.out.println("Soy la clase 2" );
+	}
+	
+	public void metodo2_clase2()
+	{
+		System.out.println("Método 2 de la Clase 2");
+	}
+	
+
+}
