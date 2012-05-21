327db04adf5938356526af2e666467fd9e426b6e
cmiranda <obando.mo.miguel@gmail.com>
2012-05-21 13:38:07 +0200

    TERCER COMMIT : He añadido el método "método_rama_master_clase2"

--- cmiranda/fuentes/clase2.java
+++ cmiranda/fuentes/clase2.java
@@ -11,5 +11,10 @@ public class clase2 {
 		System.out.println("Método 2 de la Clase 2");
 	}
 	
+	public void metodo_rama_master_clase2()
+	{
+		System.out.println("Método de la rama Master de la Clase 2");
+	}
+	
 
 }
